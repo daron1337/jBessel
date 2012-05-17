8c04c05675b6f06ab36c2cf774acbc585dbfcde5
DaroN1337 <simone.manini@gmail.com>
2012-05-17 12:31:43 +0200

    fixed documentation

--- README.txt
+++ README.txt
@@ -1,5 +1,11 @@
 This cython module computes bessel functions of the first kind (J) for integers order n (0,1,2).
-Once it has been compiled, you can import it as a standard python module.
+Once it has been installed, you can import it as a standard python module.
+Bessel functions of the first kind, denoted as Ja(x), are solutions of Bessel's differential 
+equation that are finite at the origin (x = 0) for integer a, and diverge as x approaches zero 
+for negative non-integer a. The solution type (e.g.,integer or non-integer) and normalization of 
+Ja(x) are defined by its properties below. It is possible to define the function by its Taylor 
+series expansion around x = 0.
+
 Bessel functions for python are only supported by SciPy package.
 Windows users: please download and install Scipy package.
 
@@ -10,10 +16,7 @@ Cython (http://cython.org/#download)
 
 INSTALLATION HOW TO
 
-Compiling cython code using distutils:
-(http://docs.cython.org/src/quickstart/build.html)
-
-To build, run:
-python setup.py build_ext --inplace. 
+To install, run:
+python setup.py install 
 Then simply start a Python session and do:
 from Bessel import jBessel
--- setup.py
+++ setup.py
@@ -1,11 +1,56 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
 
-ext_modules = [Extension("Bessel", ["Bessel.pyx"])]
 
-setup(
-  name = 'Bessel function app',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
-)
+CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: BSD License",
+               "Operating System :: Linux/MacOsX",
+               "Programming Language :: Cython",
+               "Programming Language :: Python",
+               "Topic :: Scientific/Engineering"]
+
+# Description
+description = "Bessel functions of the first kind written in Cython"
+fid = file('README.txt', 'r')
+long_description = fid.read()
+fid.close()
+idx = max(0, long_description.find("Bessel functions of the first kind"))
+long_description = long_description[idx:]
+
+NAME                = 'jBessel'
+MAINTAINER          = "Simone Manini"
+MAINTAINER_EMAIL    = "simone.manini@gmail.com"
+DESCRIPTION         = description
+LONG_DESCRIPTION    = long_description
+URL                 = "https://github.com/daron1337/jBessel"
+DOWNLOAD_URL        = "http://pypi.python.org/pypi/jBessel"
+LICENSE             = "BSD"
+CLASSIFIERS         = CLASSIFIERS
+AUTHOR              = "Simone Manini"
+AUTHOR_EMAIL        = "simone.manini@gmail.com"
+PLATFORMS           = "Linux/MacOsX"
+ISRELEASED          = False
+VERSION             = '0.1'
+
+setup(name=NAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      classifiers=CLASSIFIERS,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      platforms=PLATFORMS,
+      version=VERSION,
+      cmdclass = {'build_ext': build_ext},
+      ext_modules = [Extension("Bessel", ["Bessel.pyx"])]
+     )
